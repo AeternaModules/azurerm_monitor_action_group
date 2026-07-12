120618e51c0353da6c38d4de4a395a72ce147a25
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,73 +1,69 @@
-output "monitor_action_groups" {
-  description = "All monitor_action_group resources"
-  value       = azurerm_monitor_action_group.monitor_action_groups
-}
 output "monitor_action_groups_arm_role_receiver" {
-  description = "List of arm_role_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.arm_role_receiver]
+  description = "Map of arm_role_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.arm_role_receiver }
 }
 output "monitor_action_groups_automation_runbook_receiver" {
-  description = "List of automation_runbook_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.automation_runbook_receiver]
+  description = "Map of automation_runbook_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.automation_runbook_receiver }
 }
 output "monitor_action_groups_azure_app_push_receiver" {
-  description = "List of azure_app_push_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.azure_app_push_receiver]
+  description = "Map of azure_app_push_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.azure_app_push_receiver }
 }
 output "monitor_action_groups_azure_function_receiver" {
-  description = "List of azure_function_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.azure_function_receiver]
+  description = "Map of azure_function_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.azure_function_receiver }
 }
 output "monitor_action_groups_email_receiver" {
-  description = "List of email_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.email_receiver]
+  description = "Map of email_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.email_receiver }
 }
 output "monitor_action_groups_enabled" {
-  description = "List of enabled values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.enabled]
+  description = "Map of enabled values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.enabled }
 }
 output "monitor_action_groups_event_hub_receiver" {
-  description = "List of event_hub_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.event_hub_receiver]
+  description = "Map of event_hub_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.event_hub_receiver }
 }
 output "monitor_action_groups_itsm_receiver" {
-  description = "List of itsm_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.itsm_receiver]
+  description = "Map of itsm_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.itsm_receiver }
 }
 output "monitor_action_groups_location" {
-  description = "List of location values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.location]
+  description = "Map of location values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.location }
 }
 output "monitor_action_groups_logic_app_receiver" {
-  description = "List of logic_app_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.logic_app_receiver]
+  description = "Map of logic_app_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.logic_app_receiver }
 }
 output "monitor_action_groups_name" {
-  description = "List of name values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.name]
+  description = "Map of name values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.name }
 }
 output "monitor_action_groups_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.resource_group_name }
 }
 output "monitor_action_groups_short_name" {
-  description = "List of short_name values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.short_name]
+  description = "Map of short_name values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.short_name }
 }
 output "monitor_action_groups_sms_receiver" {
-  description = "List of sms_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.sms_receiver]
+  description = "Map of sms_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.sms_receiver }
 }
 output "monitor_action_groups_tags" {
-  description = "List of tags values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.tags]
+  description = "Map of tags values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.tags }
 }
 output "monitor_action_groups_voice_receiver" {
-  description = "List of voice_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.voice_receiver]
+  description = "Map of voice_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.voice_receiver }
 }
 output "monitor_action_groups_webhook_receiver" {
-  description = "List of webhook_receiver values across all monitor_action_groups"
-  value       = [for k, v in azurerm_monitor_action_group.monitor_action_groups : v.webhook_receiver]
+  description = "Map of webhook_receiver values across all monitor_action_groups, keyed the same as var.monitor_action_groups"
+  value       = { for k, v in azurerm_monitor_action_group.monitor_action_groups : k => v.webhook_receiver }
 }
 
