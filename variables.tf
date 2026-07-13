c060eb0ada8d360866a687f4f34f9c9e46465d7f
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:35:33 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -74,8 +74,8 @@ EOT
     name                = string
     resource_group_name = string
     short_name          = string
-    enabled             = optional(bool)   # Default: true
-    location            = optional(string) # Default: "global"
+    enabled             = optional(bool)
+    location            = optional(string)
     tags                = optional(map(string))
     arm_role_receiver = optional(list(object({
       name                    = string
@@ -88,7 +88,7 @@ EOT
       name                    = string
       runbook_name            = string
       service_uri             = string
-      use_common_alert_schema = optional(bool) # Default: false
+      use_common_alert_schema = optional(bool)
       webhook_resource_id     = string
     })))
     azure_app_push_receiver = optional(list(object({
